026d8f77d18de06f0e7afef7249ba2c1f0ccacac
Rodrigo <rodrigo@Rodrigos-iMac.local>
2022-09-27 20:32:20 -0500

    prueba eliminar nodo

--- pilas_test.java
+++ pilas_test.java
@@ -3,18 +3,20 @@ public class pilas_test
     public static void main(String[] args)
     {
         pila miPila = new pila();
+        miPila.insertarNodo(10);
+        miPila.insertarNodo(20);
+        miPila.insertarNodo(30);
+        miPila.insertarNodo(40);
         miPila.insertarNodo(50);
-        miPila.insertarNodo(45);
-        miPila.insertarNodo(56);
-        miPila.insertarNodo(34);
-        miPila.insertarNodo(45);
 
         miPila.mostrarValores();
 
         System.out.println(miPila.tamanioPila());
 
-        miPila.eliminarNodo();
+        System.out.println(miPila.eliminarNodo());
 
         System.out.println(miPila.tamanioPila());
+
+        miPila.mostrarValores();
     }
 }
